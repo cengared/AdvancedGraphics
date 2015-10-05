de887ed2cf1ab67ed98edc66bcc89d08e2a9af12
Daniel Stokoe <ma301ds@gold.ac.uk>
2015-10-05 10:51:46 +0100

    End of lab1

---
+++ AdvancedGraphics/Assets/testSphere.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var xSpeed : float = 1.0f;
+var ySpeed : float = 1.0f;
+var zSpeed : float = 1.0f;
+
+function Start () {
+	
+
+}
+
+function Update () {
+	transform.Translate(xSpeed*Time.deltaTime, ySpeed*Time.deltaTime, zSpeed*Time.deltaTime);
+}
